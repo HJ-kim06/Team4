f86f2121220643c29d6e689ebf1a590d04a9e604
zleda9 <zleda9@naver.com>
2020-02-11 09:45:42 +0900

    kdjbnsjdkn

---
+++ java-works/2020_01_21/src/Sample01.java
@@ -0,0 +1,16 @@
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello~");
+		System.out.println();
+		System.out.print("이름 : 강정민\n");
+		System.out.print("전공 : 영어영문\n");
+		System.out.print("나이 : 29세\n");
+		System.out.println();
+		System.out.println("기생충-조여정");
+
+
+	}
+
+}
